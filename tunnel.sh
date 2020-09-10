ec4763e07329619dd77171807a864a485b0e5720
Tim Walls <tim.walls@snowgoons.com>
2020-09-10 23:58:30 +0300

    Enabled server keepalives on the SSH tunnel

--- tunnel.sh
+++ tunnel.sh
@@ -4,7 +4,7 @@ cp /usr/local/tunnel/cert/clientcert.pem /tmp/clientcert.pem
 chmod 600 /tmp/clientcert.pem
 
 while [ 1 ] ; do
-
-  ssh -o StrictHostKeyChecking=accept-new -g -N -R *:${LISTENPORT}:${HOST}:${PORT} -i /tmp/clientcert.pem ${USER}@${REMOTEHOST}
+  echo "[`date`] Connecting SSH tunnel ${REMOTEHOST}:${LISTENPORT} => ${HOST}:${PORT}"
+  ssh -o ServerAliveInterval=15 -o StrictHostKeyChecking=accept-new -g -N -R *:${LISTENPORT}:${HOST}:${PORT} -i /tmp/clientcert.pem ${USER}@${REMOTEHOST}
   sleep 5
 done
