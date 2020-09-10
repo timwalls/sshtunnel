a6e7a227e1ab5056502910e0662389408f8c1588
Tim Walls <tim.walls@snowgoons.com>
2020-09-10 21:36:24 +0300

    Get around SSH's dumb attempts to make things secure.  Like I want secure!

--- tunnel.sh
+++ tunnel.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+cp /usr/local/tunnel/cert/clientcert.pem /tmp/clientcert.pem
+chmod 600 /tmp/clientcert.pem
+
 while [ 1 ] ; do
 
-  ssh -g -N -R ${LISTENPORT}:${HOST}:${PORT} -i /usr/local/tunnel/cert/clientcert.pem ${USER}@${REMOTEHOST}
+  ssh -o StrictHostKeyChecking=accept-new -g -N -R ${LISTENPORT}:${HOST}:${PORT} -i /tmp/clientcert.pem ${USER}@${REMOTEHOST}
   sleep 5
 done
